b87306ffbbe997e4bc8f03856b329f625df0509e
Cairo <oriac.world2017@gmail.com>
2024-02-02 13:59:20 +0100

    Add DTOs

--- GameStore/GameStore.API/Controllers/CatalogController.cs
+++ GameStore/GameStore.API/Controllers/CatalogController.cs
@@ -25,9 +25,50 @@ public class CatalogController(IGameRepository gameRepository) : ControllerBase
     }
 
     [HttpPost]
-    public IActionResult Post(Game game)
+    public IActionResult Post(GameForCreateDto gameForCreate)
     {
+        var game = new Game
+        {
+            Name = gameForCreate.Name,
+            Description = gameForCreate.Description,
+            Price = gameForCreate.Price
+        };
+
         this.gameRepository.AddGame(game);
+        
         return CreatedAtAction(nameof(Get), new { id = game.Id }, game);
     }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(string id, GameForUpdateDto gameForUpdate)
+    {
+        var game = this.gameRepository.GetGame(id);
+
+        if (game == null)
+        {
+            return NotFound();
+        }
+
+        game.Name = gameForUpdate.Name;
+        game.Description = gameForUpdate.Description;
+        game.Price = gameForUpdate.Price;
+
+        this.gameRepository.UpdateGame(game);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(string id)
+    {        
+        var game = this.gameRepository.GetGame(id);
+
+        if (game == null){
+            return NotFound();
+        }
+
+        this.gameRepository.DeleteGame(id);
+
+        return NoContent();
+    }    
 }
